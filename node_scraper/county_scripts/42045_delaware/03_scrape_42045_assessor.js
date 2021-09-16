b455b7b1d7b911c79189cec6221ceee81f86051a
Poor Richard <41810749+PoRich@users.noreplay.github.com>
2021-09-16 15:00:45 -0400

    chester county

--- node_scraper/county_scripts/42045_delaware/03_scrape_42045_assessor.js
+++ node_scraper/county_scripts/42045_delaware/03_scrape_42045_assessor.js
@@ -79,21 +79,89 @@ async function batch_process_parcel_nums(re_pattern){
 }
 
 
+
+async function scrape_batch(re_pattern, _re_start, browser){
+    var parcel_numbers = ['foobar']; // placeholder to start while loop 
+    // let period_regex = new RegExp('\\.$'); // Some parcel numbers incorrectly end in a period
+    
+        while (parcel_numbers.length > 0) {
+            await ScrapeTools.sleep(ScrapeTools.rand_num(1000,3000));
+            var page = await browser.newPage();
+            
+            page = await ScrapeTools.preparePageForTests(page);
+            page = await ScrapeTools.blockResources(page, ['font', 'media', 'image', 'other']);
+            await page.exposeFunction('zipObject', ScrapeTools.zipObject); // required for getTableData function
+        
+            var parcel_num_query = await db.query('select array(select parcel_num from pcl_data.c42045_gis where parcel_num is not null \
+                and parcel_num ~* $1 except select parcel_num from pcl_data.c42045_assessor where raw_data is not null);', [re_pattern]); 
+            
+            parcel_nums = parcel_num_query['rows'][0]['array'];
+            
+            if (parcel_nums?.length === 0){
+                console.log(`completed all gispins in regex pattern: ${re_pattern}`)
+                await page.close();
+                return 0;
+            } else if (parcel_nums == null){
+                console.log(`no in matches for regex pattern: ${re_pattern}`)
+                await page.close();
+                return 0;
+            }
+            // Filter parcel numbers (when multithreading)
+            // parcel_numbers = parcel_numbers.filter(x => re_pattern.test(x)) // match regex pattern
+            console.log(`Launching new browser tab ${re_pattern} | parcel_nums length ${parcel_nums?.length}...`);    
+
+            while (true){ // break statement in catch error logic; 
+                try{
+                    let parcel_number = parcel_nums.pop();
+                    // console.log(`re_pattern: ${re_pattern} -> gispin ${gispin} -> parcel_number ${parcel_number}`);
+                    var p = await scrape_assessor(parcel_number, page);
+                    
+                    if (p === -1){ // -1 is error for when API is blocked due to Over Limit 
+                        console.log(`scrape_assessor error code ${p} -> Restarting Browswer (parcel_number: ${parcel_number})`);
+                        await browser.close();
+                        await ScrapeTools.sleep(ScrapeTools.rand_num(3000, 5000));
+                        run(_re_start);
+                        break;
+                    } else if (p < -1){
+                        console.log(`Warning: scrape_assessor error code ${p} -> (parcel_number: ${parcel_number})`);
+                    }
+
+                    // save payload 
+                    var r = await db.query(`INSERT INTO pcl_data.c42045_assessor(parcel_num, raw_data) \
+                        VALUES ($1, $2::JSONB) ON CONFLICT (parcel_num) 
+                        DO UPDATE set raw_data = EXCLUDED.raw_data
+                        RETURNING parcel_num`, [parcel_number, p]);
+                    
+                    // console.log(`${parcel_number} Payload - ${JSON.stringify(p)}`);     
+                    console.log(`${ScrapeTools.getDateTime()} - Saved payload ${p?.parcel !== null} - Parcel Number: ${JSON.stringify(r['rows'][0]['parcel_num'], null, '\t')}`)
+                    await ScrapeTools.sleep(ScrapeTools.rand_num(100,1000));
+                } catch (e){
+                    console.log(`Scrape_batch error -> Breaking Loop, Closing Page | error message: ${e}`)
+                    await page.close();
+                    break;
+                }
+            }
+        }
+        return 0; // successfully processed all parcel_numbers
+}
+
+
 async function scrape_assessor(pcl_num, page){
     // Create and navigate to new page 
     // console.log(`Open target page for parcel_number: ${pcl_num}`);
     var url = `http://delcorealestate.co.delaware.pa.us/PT/Datalets/PrintDatalet.aspx?pin=${pcl_num}&gsp=PROFILEALL_PUB&taxyear=2021&jur=023&ownseq=0&card=1&roll=REAL&State=1&item=1&items=-1&all=all&ranks=Datalet`;
     
-    //console.log(`Open url: ${url}`);
+    console.log(`Open url: ${url}`);
     try{
         await page.goto(url,  {waitUntil: 'load', timeout: 60000})
         // console.log(`page loaded`);
 
         const actual_url = await page.url();
-        // console.log(`actual_url ${actual_url}`);
+        console.log(`actual_url ${actual_url}`);
 
         if (actual_url.includes('OverLimit.aspx')){
             console.log(`************************ API blocked on parcel number ${pcl_num} overlimit, restarting browser ************************`)
+            console.log(actual_url)
             return -1; 
         }
         // console.log(`Not overlimit...`)
@@ -138,98 +206,28 @@ async function scrape_assessor(pcl_num, page){
 
         return _p;
     } catch(e){
-        try{
-            await page.screenshot({path: `./screenshots/42045_${ScrapeTools.getDateTime()}_${pcl_num}_request_err.png`, fullPage: true});
-            console.log(`scrape_assessor failed to scrape page on parcel number : ${pcl_num} | error_message: ${e}`)
-            return -2;
-        } catch(e){
-            console.log(`Unable to capture screenshot on parcel number : ${pcl_num} | error message: ${e}`) 
-            return -3;
-        }
+        // await page.screenshot({path: `./screenshots/42045_${ScrapeTools.getDateTime()}_${pcl_num}_request_err.png`, fullPage: true});
+        console.log(`scrape_assessor failed to scrape page on parcel number : ${pcl_num} | error_message: ${e}`)
+        return -2;
     }
-}
-
-async function scrape_batch(re_pattern, _re_start, browser){
-    var parcel_numbers = ['foobar']; // placeholder to start while loop 
-    // let period_regex = new RegExp('\\.$'); // Some parcel numbers incorrectly end in a period
-    
-        while (parcel_numbers.length > 0) {
-            await ScrapeTools.sleep(ScrapeTools.rand_num(1000,3000));
-            var page = await browser.newPage();
-            
-            page = await ScrapeTools.preparePageForTests(page);
-            page = await ScrapeTools.blockResources(page, ['font', 'media', 'image', 'other']);
-            await page.exposeFunction('zipObject', ScrapeTools.zipObject); // required for getTableData function
-        
-            var parcel_num_query = await db.query('select array(select parcel_num from pcl_data.c42045_gis where parcel_num is not null \
-                and parcel_num ~* $1 except select parcel_num from pcl_data.c42045_assessor where raw_data is not null as a);', [re_pattern]); 
-            
-            parcel_nums = parcel_num_query['rows'][0]['array'];
-            
-            if (parcel_nums?.length === 0){
-                console.log(`completed all gispins in regex pattern: ${re_pattern}`)
-                await page.close();
-                return 0;
-            } else if (parcel_nums == null){
-                console.log(`no in matches for regex pattern: ${re_pattern}`)
-                await page.close();
-                return 0;
-            }
-            // Filter parcel numbers (when multithreading)
-            // parcel_numbers = parcel_numbers.filter(x => re_pattern.test(x)) // match regex pattern
-            console.log(`Launching new browser tab ${re_pattern} | parcel_nums length ${parcel_nums?.length}...`);    
-
-            while (true){ // break statement in catch error logic; 
-                try{
-                    let parcel_number = parcel_nums.pop();
-                    // console.log(`re_pattern: ${re_pattern} -> gispin ${gispin} -> parcel_number ${parcel_number}`);
-                    var p = await scrape_assessor(parcel_number, page);
-                    
-                    if (p = -1){ // -1 is error for when API is blocked due to Over Limit 
-                        console.log(`scrape_assessor error code ${p} -> Restarting Browswer (parcel_number: ${parcel_number})`);
-                        await browser.close();
-                        await ScrapeTools.sleep(ScrapeTools.rand_num(3000, 5000));
-                        run(_re_start);
-                        break;
-                    } else if (p < -1){
-                        console.log(`Warning: scrape_assessor error code ${p} -> (parcel_number: ${parcel_number})`);
-                    }
-
-                    // save payload 
-                    var r = await db.query(`INSERT INTO pcl_data.c42045_assessor(parcel_num, raw_data) \
-                        VALUES ($1, $2::JSONB) ON CONFLICT (parcel_num) 
-                        DO UPDATE set raw_data = EXCLUDED.raw_data
-                        RETURNING parcel_num`, [parcel_number, p]);
-                    
-                    // console.log(`${parcel_number} Payload - ${JSON.stringify(p)}`);     
-                    console.log(`${ScrapeTools.getDateTime()} - Saved payload ${p?.parcel !== null} - Parcel Number: ${JSON.stringify(r['rows'][0]['parcel_num'], null, '\t')}`)
-                    await ScrapeTools.sleep(ScrapeTools.rand_num(100,1000));
-                } catch (e){
-                    console.log(`Scrape_batch error -> Breaking Loop, Closing Page | error message: ${e}`)
-                    await page.close();
-                    break;
-                }
-            }
-        }
-        return 0; // successfully processed all parcel_numbers
 }
 
 
 
 // =================== RUN FUNCTION =================== 
 // scrapes parcel numbers that match the re_pattern (to allow multi-threading)
-async function run(_re_start, stage='get_parcel_nums'){    
+async function run(_re_start, stage){    
     var concurrent_tabs = 10;
     const upper_limit = _re_start + concurrent_tabs;
-    if (stage !== 'get_parcel_nums'){
+    if (stage === 'get_parcels'){
         // Start browser, prep use-agent 
         console.log(`Launching browser with regex start: ${_re_start}`);
         const browser = await puppeteer.launch({ headless: false, slowMo: 0 }); 
         for (i=_re_start; i<(upper_limit); i++){ // run 10 tabs/pages at once 
             let _re_string = i < 10 ? `^0${i}` : `^${i}`; // number -> string (add leading zero if < 10)
-            scrape_batch(_re_string, re_start, browser)
+            scrape_batch(_re_string, _re_start, browser)
         }
-    } else {
+    } else if (stage === 'get_parcel_nums') {
         for (i=_re_start; i<(upper_limit); i++){ // run 10 tabs/pages at once 
             let _re_string = i < 10 ? `^0${i}` : `^${i}`; // number -> string (add leading zero if < 10)
             batch_process_parcel_nums(_re_string)
@@ -242,10 +240,12 @@ async function run(_re_start, stage='get_parcel_nums'){
 // db.query('Create Table pcl_data.c42045_assessor (gispin TEXT, parcel_num TEXT UNIQUE, raw_data JSONB);')
 
 // input is the argument given in the command line
+
 // STEP 1 - Get parcel numbers from gispins 
-run(process.argv[2] ? parseInt(process.argv[2]) : 0, 'get_parcel_nums'); // increment this by 10 for each run of the script 
+// run(process.argv[2] ? parseInt(process.argv[2]) : 0, 'get_parcel_nums'); // increment this by 10 for each run of the script 
+
 // STEP 2 - Get parcel data from parcel numbers
-// run(process.argv[2] ? parseInt(process.argv[2]) : 0, 'get_parcels'); // increment this by 10 for each run of the script 
+run(process.argv[2] ? parseInt(process.argv[2]) : 0, 'get_parcels'); // increment this by 10 for each run of the script 
 
 
 
